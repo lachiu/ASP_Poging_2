28a41b918f3a41bec4f41a014764fe142fa76da5
lachiu <mathieu.van.daele@hotmail.com>
2023-08-23 21:06:09 +0200

    Added infocontroller

---
+++ Controllers/InfoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VoorraadSysteem.Controllers
+{
+	public class InfoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
